ecf4fe46f0c1b41be9e003509127fa530a23ea05
lirangazer <lirangazer@gmail.com>
2021-02-08 21:48:51 +0200

    8/2/21 : SignupPage fix view

--- app/src/main/java/com/example/prasonalhealthassistance/SignupPage.java
+++ app/src/main/java/com/example/prasonalhealthassistance/SignupPage.java
@@ -10,5 +10,15 @@ public class SignupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+//        findViewById(R.id.signupSave).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                EditText editUserName = (EditText) findViewById(R.id.signupUserName);
+//                String userName = editUserName.getText().toString();
+//                EditText editEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+//                String email = editEmail.getText().toString();
+//                EditText editPassword = (EditText) findViewById(R.id.textPasswordSignup);
+//                String password = editPassword.getText().toString();
+//            }});
     }
 }
