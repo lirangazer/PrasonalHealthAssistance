5c6fb3fada9bc3780a6e1c43e88faf64f5597c31
lirangazer <lirangazer@gmail.com>
2021-02-08 22:27:17 +0200

    8/2/21 : EOD add Email and Password validate

--- app/src/main/java/com/example/prasonalhealthassistance/SignupPage.java
+++ app/src/main/java/com/example/prasonalhealthassistance/SignupPage.java
@@ -1,8 +1,14 @@
 package com.example.prasonalhealthassistance;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupPage extends AppCompatActivity {
 
@@ -10,15 +16,32 @@ public class SignupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
-//        findViewById(R.id.signupSave).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                EditText editUserName = (EditText) findViewById(R.id.signupUserName);
-//                String userName = editUserName.getText().toString();
-//                EditText editEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
-//                String email = editEmail.getText().toString();
-//                EditText editPassword = (EditText) findViewById(R.id.textPasswordSignup);
-//                String password = editPassword.getText().toString();
-//            }});
+        findViewById(R.id.signupSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editUserName = (EditText) findViewById(R.id.signupUserName);
+                String userName = editUserName.getText().toString();
+                EditText editEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+                String email = editEmail.getText().toString();
+                if(!isEmailValid(email)){
+                    Toast.makeText(getApplicationContext(),"Invalid Email" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                EditText editPassword = (EditText) findViewById(R.id.textPasswordSignup);
+                String password = editPassword.getText().toString();
+                EditText editPassword1 = (EditText) findViewById(R.id.textPassword2Signup);
+                String password1 = editPassword1.getText().toString();
+                if(!(password.equals(password1)) || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Invalid Password or password don't match" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }});
+    }
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
