815c7c1f7ee72841d5414770701fab1b0f076fdf
lirangazer <lirangazer@gmail.com>
2021-02-18 18:15:30 +0200

    18.2.21: add favbutton change the UI

--- app/src/main/java/com/example/prasonalhealthassistance/LoginPage.java
+++ app/src/main/java/com/example/prasonalhealthassistance/LoginPage.java
@@ -2,9 +2,7 @@ package com.example.prasonalhealthassistance;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
--- app/src/main/java/com/example/prasonalhealthassistance/RecyclerAdapter.java
+++ app/src/main/java/com/example/prasonalhealthassistance/RecyclerAdapter.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -17,7 +21,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter <RecyclerAdapter.ViewH
 
     private Context context;
     private List<RecyclerUtils> recyclerUtils;
-
+    //private List<RecyclerUtils> favProducet;
+    //DatabaseReference favouriteRef;
+    //FirebaseDatabase database=FirebaseDatabase.getInstance();
     public RecyclerAdapter(Context context, List recyclerUtils) {
         this.context = context;
         this.recyclerUtils = recyclerUtils;
@@ -35,9 +41,44 @@ public class RecyclerAdapter extends RecyclerView.Adapter <RecyclerAdapter.ViewH
         holder.itemView.setTag(recyclerUtils.get(position));
         RecyclerUtils healthProperties = recyclerUtils.get(position);
 
+        final boolean[] favFlag = {true};
+        holder.favButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(favFlag[0]) {
+                    holder.favButton.setBackgroundResource(R.drawable.ic_baseline_add_task_green_24);
+                    Toast.makeText(context.getApplicationContext(), "The activity add to fav", Toast.LENGTH_SHORT).show();
+                    favFlag[0] =false;
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference favButton = database.getReference("Users").child("liran").child("product").child("description");
+                    String a="a";
+                    favButton.setValue(healthProperties.getTitleName());
+//                    favButton.addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                            String value = snapshot.getValue(String.class);
+//                            Toast.makeText(context.getApplicationContext(), "value is "+ value ,Toast.LENGTH_SHORT).show();
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError error) {
+//
+//                        }
+//                    });
+                    }
+                else {
+                    holder.favButton.setBackgroundResource(R.drawable.ic_baseline_add_task_gray_24);
+                    Toast.makeText(context.getApplicationContext(), "The activity remove to fav", Toast.LENGTH_SHORT).show();
+                    favFlag[0] =true;
+                }
+
+            }
+        });
+
         holder.title.setText(healthProperties.getTitleName());
         holder.description.setText(healthProperties.getDescription());
         holder.imageProfile.setImageResource(healthProperties.getImageProfile());
+
     }
 
     @Override
@@ -50,6 +91,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter <RecyclerAdapter.ViewH
         public TextView title;
         public TextView description;
         public ImageView imageProfile;
+        public Button favButton;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -57,6 +100,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter <RecyclerAdapter.ViewH
             title = itemView.findViewById(R.id.Nametxt);
             description =  itemView.findViewById(R.id.Descriptiontxt);
             imageProfile = itemView.findViewById(R.id.userImg);
+            favButton = itemView.findViewById(R.id.fav);
+
+
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
